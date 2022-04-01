3702f3198af25d81ed0326e1ce9236fb3384fbae
Yashas D B <yashasbasavaraj@gmail.com>
2022-04-01 18:08:24 +0530

    init

--- udemy-clone/src/App.js
+++ udemy-clone/src/App.js
@@ -1,11 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Landigpage } from "./Components/LandingPage/Landin";
+import Payment from "./Components/Payment/Payment";
+
+import Wishlist from "./Components/Wishlist/Wishlist";
 
 function App() {
   return (
     <div className="App">
       <Landigpage />
+      <Payment />
+      {/* <Wishlist /> */}
     </div>
   );
 }
---
+++ udemy-clone/src/Components/Wishlist/Wishlist.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./wishlist.css"
+// import Header from "../Header/Header"
+
+const Wishlist = () => {
+  return (
+    <div>
+
+         {/* <Header />  */}
+
+         <div className='wishlistDiv'>
+              
+         </div>
+    </div>
+  )
+}
+
+export default Wishlist
