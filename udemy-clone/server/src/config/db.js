ff59db62c41552d1dcaa5d86dbf1c57d6a0d6684
Yash Raj <95868808+yashrajillusion@users.noreply.github.com>
2022-04-05 13:43:50 +0530

    Update db.js

--- udemy-clone/server/src/config/db.js
+++ udemy-clone/server/src/config/db.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose");
 
 module.exports = () => {
   return mongoose.connect(
-    "mongodb+srv://yash:yash123@yash.z6ajm.mongodb.net/udemy?retryWrites=true&w=majority"
+    //////mongo link
   );
 };
