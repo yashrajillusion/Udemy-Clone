625d724ed6910555d3465b1ba9c378c0b3ed3e96
yashrajillusion <yashrajillusion@gmail.com>
2023-06-18 11:41:18 +0530

    Updated Server Infra for deployment

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 module.exports = () => {
-  return mongoose.connect(
-    //////mongo link
-  );
+  return mongoose.connect(process.env.MONGO_URL);
 };
